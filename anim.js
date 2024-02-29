927a2096b1d76e95919dd216eea7fb04ee920267
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2024-02-29 21:41:29 +0100

    add new function jquery

--- anim.js
+++ anim.js
@@ -62,3 +62,23 @@ function hexToRgb(hex) {
 
     return r + "," + g + "," + b;
 }
+
+
+$('#moi').on("click", function () {
+    now = $('#lightbox').css('display');
+    src = $(this).attr('src');
+    if (now == 'flex') {
+        $('#lightbox').css('display', 'none');
+    }
+
+    else {
+        $('#lightbox').css('display', 'flex');
+        $('#big').attr('src', src);
+    }
+})
+
+$(document).on("keyup", function (evenement) {
+    if (evenement.keyCode == 27) {
+        $('#lightbox').css('display', 'none');
+    }
+})
